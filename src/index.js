5957fee906935a7bbc6de49e05421b60ae60935c
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-02-10 08:48:11 -0800

    Final commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ let hideBtn = $('#close-menu');
 showBtn.on('click', () => {
     showBtn.hide();
     hideBtn.show();
+    $('#sideBarMenu').addClass('show-sideBar');
+    $('.ham-list').css('display', 'block');
 })
 
 hideBtn.on('click', () => {
     showBtn.show();
     hideBtn.hide();
+    $('#sideBarMenu').removeClass('show-sideBar');
+    $('.ham-list').css('display', 'none');
 })
